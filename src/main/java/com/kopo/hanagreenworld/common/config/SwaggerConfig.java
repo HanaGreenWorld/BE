c92dc7a36e088730b76e1603df32a73b43b297a7
Jimin0304 <pjws0304@naver.com>
2025-09-01 17:07:44 +0900

    chore: swagger 재세팅 및 yml파일 정리

--- src/main/java/com/kopo/hanagreenworld/common/config/SwaggerConfig.java
+++ src/main/java/com/kopo/hanagreenworld/common/config/SwaggerConfig.java
@@ -5,10 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SwaggerConfig implements WebMvcConfigurer {
+public class SwaggerConfig {
 
     private static final String API_TITLE = "하나green세상 API";
     private static final String API_DESCRIPTION = "하나green세상 API 문서";
