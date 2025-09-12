b5c512fd6d80ad017eef53816a5459dc5036352c
Jimin0304 <pjws0304@naver.com>
2025-09-12 14:59:20 +0900

    chore: redis 환경변수 경로 틀린거 수정

--- src/main/java/com/kopo/hanagreenworld/common/config/RedisConfig.java
+++ src/main/java/com/kopo/hanagreenworld/common/config/RedisConfig.java
@@ -19,13 +19,13 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 @EnableCaching
 public class RedisConfig {
 
-    @Value("${spring.redis.host:localhost}")
+    @Value("${spring.data.redis.host:localhost}")
     private String redisHost;
 
-    @Value("${spring.redis.port:6379}")
+    @Value("${spring.data.redis.port:6379}")
     private int redisPort;
 
-    @Value("${spring.redis.password:}")
+    @Value("${spring.data.redis.password:}")
     private String redisPassword;
 
     @Bean
